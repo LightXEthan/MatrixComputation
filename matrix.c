9a19bbacf4dd026006b36f00978ef4608920f0a6
LightXEthan <22248878@student.uwa.edu.au>
2019-09-14 16:51:39 +0800

    Fixed file processing for multiple digit numbers.

--- matrix.c
+++ matrix.c
@@ -8,7 +8,7 @@
 #define SIZE 14
 
 extern void memError();
-extern void addElement(char *, int, int);
+extern void addElement(char *, int);
 extern int getDataType(char *);
 extern void addCSR();
 
@@ -17,8 +17,7 @@ int *coo_i;
 int *array_j;
 int nelements = 0;
 
-int *array_val_int;
-float *array_val_float;
+float *array_val;
 
 // CSR
 int *csr_rows;
@@ -39,34 +38,28 @@ void memError() {
 }
 
 // Add to COO format, arguments value, number, (row, col, value)
-void addElement(char *value, int pointer, int datatype) {
-  int num = atoi(value);
-  // TODO: Have 4 threads realloc these
+void addElement(char *value, int element) {
+  
+  float num = atof(value);
+
   coo_i = realloc(coo_i, (nelements + 1) * sizeof(int));
   if (coo_i == NULL) memError();
-  coo_i[nelements] = pointer / nrows;
+  coo_i[nelements] = element / nrows;
 
   array_j = realloc(array_j, (nelements + 1) * sizeof(int));
   if (array_j == NULL) memError();
-  array_j[nelements] = pointer % ncols;
+  array_j[nelements] = element % ncols;
 
-  if (datatype == 0) {
-    array_val_int = realloc(array_val_int, (nelements + 1) * sizeof(int));
-    if (array_val_int == NULL) memError();
-    array_val_int[nelements] = num;
-  }
-  else {
-    array_val_float = realloc(array_val_float, (nelements + 1) * sizeof(float));
-    if (array_val_float == NULL) memError();
-    array_val_float[nelements] = num;
-  }
-  
+  array_val = realloc(array_val, (nelements + 1) * sizeof(int));
+  if (array_val == NULL) memError();
+  array_val[nelements] = num;
+
   // Adds to csr array if the coo_i value changes
   if (nelements > 0 && coo_i[nelements] != coo_i[nelements - 1]) {
     addCSR();
   }
   csr_counter++;
-
+  
   nelements++;
   return;
 }
@@ -101,42 +94,24 @@ void addCSR() {
   return;
 }
 
-void scalarMultiplication(int scalari, float scalarf, int datatype,int nthreads , int parallel) {
+void scalarMultiplication(float scalar, int nthreads , int parallel) {
   if (parallel == 0) {
-    if (datatype == 0) {
-        for (int i = 0; i < nelements; i++)
-        {
-          array_val_int[i] *= scalari;
-        }
-      }
-      else {
-        for (int i = 0; i < nelements; i++)
-        {
-          array_val_float[i] *= scalarf;
-        }
-      }
+    for (int i = 0; i < nelements; i++) {
+      array_val[i] *= scalar;
+    }
   }
   if (parallel == 1) {
-    if (datatype == 0) {
-        #pragma omp parallel for num_threads(nthreads)
-        for (int i = 0; i < nelements; i++)
-        {
-          array_val_int[i] *= scalari;
-        }
-      }
-      else {
-        // TODO: parallelise
-        for (int i = 0; i < nelements; i++)
-        {
-          array_val_float[i] *= scalarf;
-        }
-      }
+    #pragma omp parallel for num_threads(nthreads)
+    for (int i = 0; i < nelements; i++)
+    {
+      array_val[i] *= scalar;
+    }
   }
   
   return;
 }
 
-int trace(int datatype, int nthreads, int parallel) {
+int trace(int nthreads, int parallel) {
 
   int total = 0;
   
@@ -145,7 +120,7 @@ int trace(int datatype, int nthreads, int parallel) {
     for (int i = 0; i < nelements; i++)
     {
       if (coo_i[i] == array_j[i]) {
-        total += array_val_int[i];
+        total += array_val[i];
       }
     }
   }
@@ -161,7 +136,7 @@ int trace(int datatype, int nthreads, int parallel) {
       for (int i = 0; i < nelements; i++)
       {
         if (coo_i[i] == array_j[i]) {
-          total += array_val_int[i];
+          total += array_val[i];
         }
       }
       results[id] = total;
@@ -179,6 +154,9 @@ int trace(int datatype, int nthreads, int parallel) {
 int main(int argc, char *argv[]) {
 
   char *filename = NULL;
+  char *filename2 = NULL;
+  int logtofile = 0;        // 1 = true, for the -l log flag, logs are outputted to a file
+  int isMultiFile = 0;      // 1 = true, for operations that require 2 matrix (2 files)
   enum operations{Scalar, Trace, Addition, Transpose, Multiply};
   enum operations op;
 
@@ -189,8 +167,7 @@ int main(int argc, char *argv[]) {
   // Time start_ps to convert matrix files
   clock_t start_p = clock();
 
-  int scalari;
-  float scalarf;
+  float scalar;
 
   for (int i = 0; i < argc; i++)
   {
@@ -198,13 +175,19 @@ int main(int argc, char *argv[]) {
       case 'f':
         filename = argv[++i];
         printf("File: %s\n", filename);
+        if (isMultiFile == 1) {
+          filename2 = argv[++i];
+          printf("File2: %s\n", filename2);
+        }
+      case 'l':
+        // logs are filed
+        logtofile = 1;
       case '-':
         if (argv[i][2] == 's' && argv[i][3] == 'c') {
           // Scalar Multiplication
           op = Scalar;
-          scalari = atoi(argv[++i]);
-          scalarf = atof(argv[i]);
-          printf("Scalar operation.\n");
+          scalar = atof(argv[++i]);
+          printf("Scalar operation. %d\n", (int) scalar);
         }
         if (argv[i][2] == 't' && argv[i][3] == 'r') {
           op = Trace;
@@ -212,6 +195,7 @@ int main(int argc, char *argv[]) {
         }
         if (argv[i][2] == 'a' && argv[i][3] == 'd') {
           op = Addition;
+          isMultiFile = 1;
           printf("Addition operation.\n");
         }
         if (argv[i][2] == 't' && argv[i][3] == 's') {
@@ -228,8 +212,14 @@ int main(int argc, char *argv[]) {
   // File data
   FILE *file = fopen(filename, "r");
   char buf[SIZE];
+  
   char databuf[7];
   int datatype = 0; // Datatype, defualt int = 0, float = 1, -1 for error
+
+  // Gets the second file
+  if (isMultiFile == 1) {
+    FILE *file = fopen(filename2, "r");
+  }
 
   // Gets the datatype from the file
   char *data = fgets(databuf, 7, file);
@@ -246,24 +236,42 @@ int main(int argc, char *argv[]) {
 
   printf("Number of Rows: %d\nNumber of Columns: %d\n", nrows, ncols);
 
-  const char s[2] = " ";
-  char *token; // Value
-  char *zero = "0";
+  const char s[2] = " "; // Splitter
+  char *pointer;         // 
+  char save[SIZE];       // Saves the element
+  int elementlen = 0;    // length of the element
 
   csr_rows = calloc(++ncsr, sizeof(int));
   csr_rows[0] = 0;
-  int pointer = 0;
+  int element = 0; // position of element
 
   while (fgets(buf, SIZE, file) != NULL) {
-    token = strtok(buf, s);
-
-    while ( token != NULL) {
-      if (*token != *zero) {
-        addElement(token, pointer, datatype);
+    //printf("Buff: %s\n", buf); #Remove
+    pointer = &buf[0];
+    
+    while (*pointer != '\0') {
+      //printf("Pointer at: [%c]\n", *pointer); #Remove
+      if ((*pointer == ' ' || *pointer == '\n') && elementlen > 0) {
+        // Add element to format
+        save[elementlen] = '\0';
+        //printf("Save: [%s]\n", save); #Remove
+        addElement(save, element++);
+        elementlen = 0;
+      } 
+      else if (*pointer == '0' && elementlen == 0) {
+        // detects if the pointer is at a zero element
+        *pointer++;
+        if (*pointer == '\0') break;
       }
-      pointer++;
-      token = strtok(NULL, s);
+      else if (*pointer != ' ') {
+        // saves the characters in the element
+        save[elementlen++] = *pointer;
+      }
+      
+      *pointer++;
+      //printf("Pointer at: %c\n", *pointer);
     }
+    //printf("Exits\n");
   }
 
   // Adds the final value(s) of CSR
@@ -273,24 +281,32 @@ int main(int argc, char *argv[]) {
   clock_t end_p = clock();
   double total_p = (double) (end_p - start_p) / CLOCKS_PER_SEC;
   printf("Time for file processing: %f\n", total_p);
-
-  // Debugging purposes
-  for (int i = 0; i < nelements; i++)
-  {
-    //printf("COO: (%d, %d, %d)\n", coo_i[i], array_j[i], array_val_int[i]);
-    //printf("CSR: (%d, %d, %d)\n", array_val_int[i], csr_rows[i+1], array_j[i]);
-  }
-
+  
   // Times the operation time
   clock_t start_o = clock();
 
   // Scalar multiplication
   if (op == Scalar) {
-    scalarMultiplication(scalari, scalarf, datatype, nthreads, parallel);
+    scalarMultiplication(scalar, nthreads, parallel);
   }
   if (op == Trace) {
-    int trace_sum = trace(datatype, nthreads, parallel);
+    int trace_sum = trace(nthreads, parallel);
     printf("Result of Trace sum: %d\n", trace_sum);
+  }
+  if (op == Addition) {
+    
+  }
+
+  // Debugging purposes #Remove
+  for (int i = 0; i < nelements; i++)
+  {
+    if (datatype == 0) {
+      //printf("COO: (%d, %d, %d)\n", coo_i[i], array_j[i], (int) array_val[i]);
+    }
+    if (datatype == 1) {
+      //printf("COO: (%d, %d, %f)\n", coo_i[i], array_j[i], array_val[i]);
+    }
+    //printf("CSR: (%d, %d, %d)\n", array_val[i], csr_rows[i+1], array_j[i]);
   }
   
 
@@ -301,10 +317,15 @@ int main(int argc, char *argv[]) {
   fclose(file);
   free(coo_i);
   free(array_j);
-
-  free(array_val_int);
-  free(array_val_float);
+  free(array_val);
   free(csr_rows);
+
+  // Logs files
+  if (logtofile == 1) {
+    FILE *fileout = fopen(strcat(filename,".out"), "w");
+    
+    fclose(fileout);
+  }
   printf("====== Log end ======\n");
 
 
@@ -313,5 +334,15 @@ int main(int argc, char *argv[]) {
   {
     printf("Hello!\n");
   }
+  */
+
+  /*
+  int b = 2;
+  float a = 0.1;
+  float c[2];
+  c[0] = a;
+  c[1] = (float) b;
+  printf("%f, %f, %d\n", c[0], c[1], (int) c[1]);
+
   */
 }
